39e9a656a91520dddfbcfcb91d6c129dea8c8009
Mohamed Kamaleldin <mohammed.kamaleldin@innosoft.com>
2025-11-16 12:12:32 +0300

    Lab Completed

--- 7-1-mongodb/server.js
+++ 7-1-mongodb/server.js
@@ -169,23 +169,50 @@
 
 import mongoose from "mongoose";
 
+ 
 // establish connection
-
+mongoose.connect("mongodb+srv://moekamaleldin7_db_user:zaq1!QAZ@cluster0.3g65uom.mongodb.net/labDB")
 
 // define schema
+const studentSchema = new mongoose.Schema({
+         name: String,
+         age: Number,
+         major: String
+      });
 
+const Student = mongoose.model("Student", studentSchema);
 
 // create document
 
+   async function createStudents(){
+      Student.insertMany([
+         { name: "Ali", age: 21, major: "CS" },
+         { name: "Sara", age: 23, major: "SE" }
+      ]);
+      console.log("Inserted");
+}
+
+createStudents();
 
 // read document
-
+ async function readStudents() {
+         const all = await Student.find();
+         console.log(all);
+      }
+      readStudents();
 
 // update document
-
+  async function updateStudent() {
+         await Student.updateOne({ name: "Ali" }, { age: 22 });
+         console.log("✅ Updated Ali");
+      }
+      updateStudent();
 
 // delete document
-
-
+ async function deleteStudent() {
+         await Student.deleteOne({ name: "Sara" });
+      console.log("✅ Deleted Sara");
+      }
+deleteStudent();
 
 
